fdce8176b4d6a4f1272bcfce4a317235f94f2c20
Tanachi <tanachige@gmail.com>
2017-10-04 12:41:36 -1000

    Added Check to budget

--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ namespace SCDashboard
             var port = ConfigurationManager.AppSettings["port"];
             var storyID = ConfigurationManager.AppSettings["story"];
             var team = ConfigurationManager.AppSettings["team"];
+            var cost = ConfigurationManager.AppSettings["cost"];
             var URL = "https://my.sharpcloud.com";
             var sc = new SharpCloudApi(userid, passwd, URL);
             // Gets Story ID from URL
@@ -39,9 +40,15 @@ namespace SCDashboard
             var teamBook = sc.StoriesTeam(team);
             var dashStory = sc.LoadStory(storyUrl);
             // Adds new attributes if story does not have it
-            dashStory.Attribute_Add("Appropriated Budget", SC.API.ComInterop.Models.Attribute.AttributeType.Numeric);
-            dashStory.Attribute_Add("New Requested Budget", SC.API.ComInterop.Models.Attribute.AttributeType.Numeric);
-            dashStory.Attribute_Add("RAG Status", SC.API.ComInterop.Models.Attribute.AttributeType.List);
+            if(dashStory.Attribute_FindByName("Appropriated Budget") == null)
+                dashStory.Attribute_Add("Appropriated Budget", SC.API.ComInterop.Models.Attribute.AttributeType.Numeric);
+            if (dashStory.Attribute_FindByName("RAG Status") == null)
+                dashStory.Attribute_Add("RAG Status", SC.API.ComInterop.Models.Attribute.AttributeType.List);
+            if (dashStory.Attribute_FindByName("New Requested Budget") == null)
+                dashStory.Attribute_Add("New Requested Budget", SC.API.ComInterop.Models.Attribute.AttributeType.Numeric);
+            
+            
+
             
             //Selected Roadmaps to insert into dashboard
             int[] maps = new int[4];
@@ -92,6 +99,46 @@ namespace SCDashboard
             }
 
             dashStory.Save();
+        }
+        void highCost(Story story, Story newStory)
+        {
+            String catName = "";
+            String newCat = "";
+            // Finds the category that contains the project
+            foreach (var cat in story.Categories)
+            {
+                var catLine = cat.Name.Split(' ');
+                // Checks to see if last word is project, if so, adds the other words to be a category for the dashboard
+                if (catLine[catLine.Length - 1] == "Projects")
+                {
+                    catName = cat.Name;
+                    String[] noProject = new String[catLine.Length - 1];
+                    Array.Copy(catLine, noProject, catLine.Length - 1);
+
+                    newCat = String.Join(" ", noProject);
+                    if (newStory.Category_FindByName(newCat) == null)
+                        newStory.Category_AddNew(newCat);
+                }
+            }
+            // Copies attribute data from team story to dashboard story
+            foreach (var item in story.Items)
+            {
+                double checkBudget = item.GetAttributeValueAsDouble(story.Attribute_FindByName("Appropriated Budget"));
+                if (item.Category.Name == catName && checkBudget > 500000)
+                {
+                    Item scItem = newStory.Item_AddNew(item.Name);
+                    scItem.Category = newStory.Category_FindByName(newCat);
+                    scItem.StartDate = Convert.ToDateTime(item.StartDate.ToString());
+                    scItem.DurationInDays = item.DurationInDays;
+                    double appBudget = item.GetAttributeValueAsDouble(story.Attribute_FindByName("Appropriated Budget"));
+                    double newBudget = item.GetAttributeValueAsDouble(story.Attribute_FindByName("New Requested Budget"));
+                    var RAG = item.GetAttributeValueAsText(story.Attribute_FindByName("RAG Status"));
+                    scItem.SetAttributeValue(newStory.Attribute_FindByName("Appropriated Budget"), appBudget);
+                    scItem.SetAttributeValue(newStory.Attribute_FindByName("New Requested Budget"), newBudget);
+                    scItem.SetAttributeValue(newStory.Attribute_FindByName("RAG Status"), RAG);
+                }
+            }
+            newStory.Save();
         }
     }
 }
